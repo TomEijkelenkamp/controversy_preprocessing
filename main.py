533452fd951889264daa55282f21c0d4cb1e3635
TomEijkelenkamp <66938518+TomEijkelenkamp@users.noreply.github.com>
2023-05-25 13:16:34 +0200

    listing script

--- main.py
+++ main.py
@@ -1,34 +1,63 @@
 import pandas as pd
 import csv
+ 
 
 
 
 def read_comments(comments):
-    
-    for idx_1, comment_list in enumerate(comments):
-        print(f"Size in comment {idx_1}: {len(comment_list)}")
-        for idx_2, comment in enumerate(comment_list):
-            item = {'text': "", 'ups': 0, 'downs': 0, 'controversy': 0}
-            print(f"Comment index {idx_1}-{idx_2}: {comment}")
-            if 'body' in comment:
-                item['text'] = comment['body']
-                print(f"Body of comment: {comment['body']}")
-            if 'ups' in comment:
-                item['ups'] = comment['ups']
-                print(f"Ups in comment: {comment['ups']}")
-            if 'downs' in comment:
-                item['downs'] = comment['downs']
-                print(f"Downs in comment: {comment['downs']}")
-            print("\n\n\n")
-            if item['ups'] > 0 and item['downs'] > 0:
-                if item['ups'] > item['downs']:
-                    item['controversy'] = item['downs'] / item['ups']
-                else:
-                    item['controversy'] = item['ups'] / item['downs']
-            write_csv(item)
+
+    for idx_2, comment in enumerate(comments):
+        global total_comments, controversial_comments
+        total_comments += 1
+        item = {'text': "", 'ups': 0, 'downs': 0, 'score': 0, 'level': 0, 'controversy': 0, 'controversiality': 0, 'created_utc': 0, 'author': ''}
+        # print(f"Comment index {idx_1}-{idx_2}: {comment}")
+        if 'body' in comment:
+            item['text'] = comment['body']
+            # print(f"Body of comment: {comment['body']}")
+        if 'ups' in comment:
+            item['ups'] = comment['ups']
+            # print(f"Ups in comment: {comment['ups']}")
+        if 'downs' in comment:
+            item['downs'] = comment['downs']
+            # print(f"Downs in comment: {comment['downs']}")
+        # print("\n\n\n")
+        
+        if item['ups'] > 0 and item['downs'] > 0:
+            if item['ups'] > item['downs']:
+                item['controversy'] = item['downs'] / item['ups']
+            else:
+                item['controversy'] = item['ups'] / item['downs']
+        
+        if 'controversiality' in comment:
+            item['controversiality'] = comment['controversiality']
+            controversial_comments += comment['controversiality']
+            # print(f"Controversiality in comment: {comment['controversiality']}")
+
+        if 'score' in comment:
+            item['score'] = comment['score']
+            # print(f"Score in comment: {comment['score']}")
+
+        if 'created_utc' in comment:
+            item['created_utc'] = comment['created_utc']
+            # print(f"Created_utc in comment: {comment['created_utc']}")
+
+        if 'author' in comment:
+            item['author'] = comment['author']
+            # print(f"Author in comment: {comment['author']}")
+
+        if 'level' in comment:
+            item['level'] = comment['level']
+            # print(f"Level in comment: {comment['level']}")
+
+        # pd.DataFrame(item, index=[0]).to_csv('data.csv', sep=';', mode='a', header=False, index=False)
+
+        if 'children' in comment:
+            read_comments(comment['children'])
+            # print(f"Children in comment: {comment['children']}")
+            
 
 
-def write_csv(new_data, field_names=['text', 'ups', 'downs', 'controversy'], filename='data.csv'):
+def write_csv(new_data, field_names=['text', 'ups', 'downs', 'score', 'level', 'controversy', 'controversiality', 'created_utc', 'author'], filename='data.csv'):
     with open(filename,'a', encoding="utf-8") as csv_file:
         dict_object = csv.DictWriter(csv_file, fieldnames=field_names) 
         dict_object.writerow(new_data)
@@ -36,11 +65,18 @@ def write_csv(new_data, field_names=['text', 'ups', 'downs', 'controversy'], fil
 
 def read_post(post):
     if 'comments' in post:
-        read_comments(post['comments'])
+        for idx_1, comment_list in enumerate(post['comments']):
+            read_comments(comment_list)
+
         
+total_comments = 0
+controversial_comments = 0
 
 data = pd.read_json('threads.jsonl', orient="records", lines=True, chunksize=1000)
 # print(data.shape)
 for idx, post in enumerate(data):
     print(idx)
     read_post(post)
+
+print(f"Total: {total_comments}")
+print(f"Controversial: {controversial_comments}")
